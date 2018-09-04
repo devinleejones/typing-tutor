c9aebfc91088bfc6ffc9e794f46da8af8062630a
Devin Lee-Jones <devinleejones@gmail.com>
2018-09-04 15:50:06 -0700

    Create an Object to represent app state, and make characters a property #2

---
+++ main.js
@@ -0,0 +1,35 @@
+var myArray = []
+
+var appState = {
+  state: false,
+  characters: myArray
+}
+
+function createCharacter(string) {
+  for (var i = 0; i < string.length; i++) {
+  var obj = {
+    letter: ''
+  }
+  obj.letter = string[i]
+  myArray.push(obj)
+  }
+  return myArray
+}
+
+createCharacter('grumpy wizards make toxic brew for the evil queen and jack')
+
+function renderCharacter(character) {
+  var $letter = document.createElement('span')
+  $letter.textContent = character.letter
+  return $letter
+}
+
+function renderAllCharacters(characters) {
+  var $mySentence = document.createElement('div')
+  for (var i = 0; i < characters.length; i++){
+    $mySentence.appendChild(renderCharacter(characters[i]))
+  }
+  return $mySentence
+}
+
+document.body.appendChild(renderAllCharacters(myArray))
